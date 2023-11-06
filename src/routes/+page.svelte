5a30a7a26f28be43da8368e631dc284a8b3c35b9
Lucas Wiese <lucas.wiese@gmx.de>
2023-11-06 21:12:23 +0100

    get the user settings => hack to get the role

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import { GET } from '$lib/api';
+	import { onMount } from 'svelte';
 	const tags = GET('/tags', {});
+
+	const myuser = fetch('/api/users/me').then((res) => res.json());
+
+	onMount(() => {});
 </script>
 
-<h1>Request to /tags</h1>
-{#await tags then value}
+<h1>Access to my user data</h1>
+{#await myuser then value}
 	<pre>
         {JSON.stringify(value, null, 2)}
     </pre>
