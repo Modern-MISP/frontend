86a4d2aa46b2c76e8602302c990edd3a7ea1874b
Lucas Wiese <lucas.wiese@gmx.de>
2023-11-09 23:33:20 +0100

    menu collapse

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,63 +11,93 @@
 	import PinOffIcon from 'svelte-material-icons/PinOff.svelte';
 	import EyeIcon from 'svelte-material-icons/EyeCircleOutline.svelte';
 	import SearchIcon from 'svelte-material-icons/Magnify.svelte';
+	import { page } from '$app/stores';
+
+	let openMenu = false;
 </script>
 
-<div class="absolute w-full h-full flex flex-row macchiato bg-base text-text">
-	<div class="bg-mantle p-2 relative rounded-md flex flex-col gap-6">
-		<div class="flex gap-3 px-6 pt-6">
-			<div class="h-10 w-10 rounded-full bg-text">.</div>
-			<h1 class="text-4xl font-bold">MISP</h1>
-		</div>
-		<div class="px-6">
-			<hr />
-		</div>
-
-		<div class="flex flex-col gap-2">
-			<h3 class="text-sm underline px-4">Fast access</h3>
-			<div class="flex gap-2 w-60 items-center p-4 hover:text-sky cursor-pointer">
-				<div class=" text-2xl rounded-full"><EventIcon /></div>
-				<span> Events/List </span>
-				<div class=" text-2xl ml-auto rounded-full"><PinOffIcon /></div>
+<div class="absolute w-[100vw] h-full flex flex-row macchiato bg-base text-text">
+	{#if openMenu}
+		<div class="bg-mantle p-2 relative rounded-md flex flex-col gap-6">
+			<div class="flex gap-3 px-6 pt-6">
+				<div class="h-10 w-10 rounded-full bg-text">.</div>
+				<h1 class="text-4xl font-bold">MISP</h1>
+			</div>
+			<div class="px-6">
+				<hr />
 			</div>
 
-			<div class="flex gap-2 w-60 items-center p-4 hover:text-sky cursor-pointer">
-				<div class=" text-2xl rounded-full"><AdminIcon /></div>
-				<span> Admin/Settings </span>
-				<div class=" text-2xl ml-auto rounded-full"><PinIcon /></div>
-			</div>
-		</div>
-		<div class="px-6">
-			<hr />
-		</div>
-		<div class="flex flex-col gap-2">
-			<div
-				class="flex gap-2 w-60 items-center bg-crust p-4 rounded-md hover:text-sky cursor-pointer"
-			>
-				<div class=" text-2xl rounded-full"><EventIcon /></div>
-				<span> Events/List </span>
-				<div class=" text-2xl ml-auto rounded-full"><ChevronDown /></div>
-			</div>
+			<div class="flex flex-col gap-2">
+				<h3 class="text-sm underline px-4">Fast access</h3>
+				<a
+					href="event"
+					class:text-sky={$page.url.href.includes('event')}
+					class="flex gap-2 w-60 items-center p-4 hover:text-sky cursor-pointer"
+				>
+					<div class=" text-2xl rounded-full"><EventIcon /></div>
+					<span> Events/List </span>
+					<div class=" text-2xl ml-auto rounded-full"><PinOffIcon /></div>
+				</a>
 
-			<div
-				class="flex gap-2 w-60 items-center bg-crust p-4 rounded-md hover:text-sky cursor-pointer"
-			>
-				<div class=" text-2xl rounded-full"><ProfileIcon /></div>
-				<span> Profile </span>
-				<div class=" text-2xl ml-auto rounded-full"><ChevronDown /></div>
+				<div class="flex gap-2 w-60 items-center p-4 hover:text-sky cursor-pointer">
+					<div class=" text-2xl rounded-full"><AdminIcon /></div>
+					<span> Admin/Settings </span>
+					<div class=" text-2xl ml-auto rounded-full"><PinIcon /></div>
+				</div>
 			</div>
-		</div>
-		<div class="mt-auto flex flex-col gap-4 p-6">
-			<hr />
-			<div class="flex justify-between">
-				<span>Version 0.0.1</span>
-				<div class="text-2xl ml-auto rounded-full hover:text-sky cursor-pointer">
-					<ChevronLeft />
+			<div class="px-6">
+				<hr />
+			</div>
+			<div class="flex flex-col gap-2">
+				<div
+					class="flex gap-2 w-60 items-center bg-crust p-4 rounded-md hover:text-sky cursor-pointer"
+				>
+					<div class=" text-2xl rounded-full"><EventIcon /></div>
+					<span> Events/List </span>
+					<div class=" text-2xl ml-auto rounded-full"><ChevronDown /></div>
+				</div>
+
+				<div
+					class="flex gap-2 w-60 items-center bg-crust p-4 rounded-md hover:text-sky cursor-pointer"
+				>
+					<div class=" text-2xl rounded-full"><ProfileIcon /></div>
+					<span> Profile </span>
+					<div class=" text-2xl ml-auto rounded-full"><ChevronDown /></div>
+				</div>
+			</div>
+			<div class="mt-auto flex flex-col gap-4 px-6 pb-4">
+				<hr />
+				<div class="flex justify-between">
+					<span>Version 0.0.1</span>
+					<button
+						on:click={() => (openMenu = false)}
+						class="text-2xl ml-auto rounded-full hover:text-sky cursor-pointer"
+					>
+						<ChevronLeft />
+					</button>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="w-full h-full">
+	{:else}
+		<div class="bg-mantle p-4 pt-8 relative rounded-md flex flex-col gap-6 items-center">
+			<div class="h-10 w-10 rounded-full bg-text">.</div>
+			<hr class="w-full" />
+			<div class=" text-2xl rounded-full"><EventIcon /></div>
+			<div class=" text-2xl rounded-full"><AdminIcon /></div>
+			<hr class="w-full" />
+			<div class=" text-2xl rounded-full"><EventIcon /></div>
+			<div class=" text-2xl rounded-full"><ProfileIcon /></div>
+			<hr class="w-full mt-auto" />
+			<button
+				on:click={() => (openMenu = true)}
+				class="text-2xl rounded-full hover:text-sky cursor-pointer"
+			>
+				<ChevronRight />
+			</button>
+		</div>
+	{/if}
+
+	<div class="grow flex flex-col h-full min-w-0">
 		<div class="bg-mantle items-center p-4 rounded-md ml-4 flex flex-row justify-between gap-4">
 			<h1 class="text-2xl">Dashboard</h1>
 			<div class="flex gap-6 items-center">
@@ -85,7 +115,7 @@
 				</div>
 			</div>
 		</div>
-		<main class="p-10 relative overflow-auto">
+		<main class="m-8 relative overflow-auto">
 			<slot />
 		</main>
 	</div>
