b2503310ccf10cd9ef6ee0258c66e5ebf6d3468e
Lucas Wiese <lucas.wiese@gmx.de>
2023-11-09 23:00:41 +0100

    simple menu mochup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,92 @@
-<div class="absolute w-full h-full flex flex-col">
-	<div class="bg-blue-500 w-full p-4 relative">
-		<ul class="flex gap-4 font-bold text-white">
-			<li>
-				<h1 class="text-white">Home</h1>
-			</li>
-			<li class="underline">
-				<a href="/user">My User</a>
-			</li>
-			<li class="underline">
-				<a href="/event">Events</a>
-			</li>
-		</ul>
+<script>
+	import ChevronLeft from 'svelte-material-icons/ChevronLeft.svelte';
+	import ChevronDown from 'svelte-material-icons/ChevronDown.svelte';
+	import ChevronUp from 'svelte-material-icons/ChevronUp.svelte';
+	import ChevronRight from 'svelte-material-icons/ChevronRight.svelte';
+	import EventIcon from 'svelte-material-icons/Calendar.svelte';
+	import AdminIcon from 'svelte-material-icons/ShieldCrown.svelte';
+	import ProfileIcon from 'svelte-material-icons/Account.svelte';
+	import ProfileCircle from 'svelte-material-icons/AccountCircle.svelte';
+	import PinIcon from 'svelte-material-icons/Pin.svelte';
+	import PinOffIcon from 'svelte-material-icons/PinOff.svelte';
+	import EyeIcon from 'svelte-material-icons/EyeCircleOutline.svelte';
+	import SearchIcon from 'svelte-material-icons/Magnify.svelte';
+</script>
+
+<div class="absolute w-full h-full flex flex-row macchiato bg-base text-text">
+	<div class="bg-mantle p-2 relative rounded-md flex flex-col gap-6">
+		<div class="flex gap-3 px-6 pt-6">
+			<div class="h-10 w-10 rounded-full bg-text">.</div>
+			<h1 class="text-4xl font-bold">MISP</h1>
+		</div>
+		<div class="px-6">
+			<hr />
+		</div>
+
+		<div class="flex flex-col gap-2">
+			<h3 class="text-sm underline px-4">Fast access</h3>
+			<div class="flex gap-2 w-60 items-center p-4 hover:text-sky cursor-pointer">
+				<div class=" text-2xl rounded-full"><EventIcon /></div>
+				<span> Events/List </span>
+				<div class=" text-2xl ml-auto rounded-full"><PinOffIcon /></div>
+			</div>
+
+			<div class="flex gap-2 w-60 items-center p-4 hover:text-sky cursor-pointer">
+				<div class=" text-2xl rounded-full"><AdminIcon /></div>
+				<span> Admin/Settings </span>
+				<div class=" text-2xl ml-auto rounded-full"><PinIcon /></div>
+			</div>
+		</div>
+		<div class="px-6">
+			<hr />
+		</div>
+		<div class="flex flex-col gap-2">
+			<div
+				class="flex gap-2 w-60 items-center bg-crust p-4 rounded-md hover:text-sky cursor-pointer"
+			>
+				<div class=" text-2xl rounded-full"><EventIcon /></div>
+				<span> Events/List </span>
+				<div class=" text-2xl ml-auto rounded-full"><ChevronDown /></div>
+			</div>
+
+			<div
+				class="flex gap-2 w-60 items-center bg-crust p-4 rounded-md hover:text-sky cursor-pointer"
+			>
+				<div class=" text-2xl rounded-full"><ProfileIcon /></div>
+				<span> Profile </span>
+				<div class=" text-2xl ml-auto rounded-full"><ChevronDown /></div>
+			</div>
+		</div>
+		<div class="mt-auto flex flex-col gap-4 p-6">
+			<hr />
+			<div class="flex justify-between">
+				<span>Version 0.0.1</span>
+				<div class="text-2xl ml-auto rounded-full hover:text-sky cursor-pointer">
+					<ChevronLeft />
+				</div>
+			</div>
+		</div>
 	</div>
-	<main class="p-10 bg-slate-700 w-full h-full text-white relative overflow-auto">
-		<slot />
-	</main>
+	<div class="w-full h-full">
+		<div class="bg-mantle items-center p-4 rounded-md ml-4 flex flex-row justify-between gap-4">
+			<h1 class="text-2xl">Dashboard</h1>
+			<div class="flex gap-6 items-center">
+				<div class=" flex gap-4 items-center p-3 w-96 bg-crust rounded-md mr-4">
+					<div class=" text-3xl rounded-full"><SearchIcon /></div>
+					<span> Search </span>
+				</div>
+				<div class="flex flex-col items-center gap-1">
+					<div class=" text-5xl rounded-full"><EyeIcon /></div>
+					<span class="text-xs">View mode</span>
+				</div>
+				<div class="flex flex-col items-center gap-1">
+					<div class=" text-5xl rounded-full"><ProfileCircle /></div>
+					<span class="text-xs">admin@admin.test</span>
+				</div>
+			</div>
+		</div>
+		<main class="p-10 relative overflow-auto">
+			<slot />
+		</main>
+	</div>
 </div>
