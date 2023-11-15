65f99cd05ee6bfdd31d68e80d3494b072ef627b2
Lucas Wiese <lucas.wiese@gmx.de>
2023-11-15 15:03:52 +0100

    Add ts-extras package and added maintenance config

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,12 +1,34 @@
-import createClient from 'openapi-fetch';
+import createClient, { type FetchResponse } from 'openapi-fetch';
+import { objectEntries } from 'ts-extras';
 import type { paths } from './misp'; // generated from openapi-typescript
-import { PUBLIC_MISP_KEY } from '$env/static/public';
+import {
+	PUBLIC_MISP_API_ENDPOINT,
+	PUBLIC_MISP_KEY,
+	PUBLIC_REST_DISABLED
+} from '$env/static/public';
+import { error } from '@sveltejs/kit';
 
-export const { DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE } = createClient<paths>({
-	baseUrl: 'http://localhost',
-	headers: {
-		Authorization: PUBLIC_MISP_KEY,
-		Accept: 'application/json',
-		'Content-type': 'application/json'
+async function defaultDisabled(): Promise<FetchResponse<object>> {
+	throw error(503, 'The REST method you try to use is disabled');
+}
+const api = {
+	...createClient<paths>({
+		baseUrl: PUBLIC_MISP_API_ENDPOINT,
+		headers: {
+			Authorization: PUBLIC_MISP_KEY,
+			Accept: 'application/json',
+			'Content-type': 'application/json'
+		}
+	}),
+	disableRESTMethods(
+		config: { [k in keyof ReturnType<typeof createClient<paths>>]?: boolean },
+		disabled = defaultDisabled
+	) {
+		objectEntries(config).forEach(([key, value]) => {
+			if (value) this[key] = disabled;
+		});
 	}
-});
+};
+
+if (PUBLIC_REST_DISABLED) api.disableRESTMethods(JSON.parse(PUBLIC_REST_DISABLED));
+export const { GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE } = api;
