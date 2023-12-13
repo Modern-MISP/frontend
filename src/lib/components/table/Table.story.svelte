dc2d1928aea269278ed6226d301c85a5f4da7f3c
Lucas Wiese <lucas.wiese@gmx.de>
2023-12-13 17:54:50 +0100

    working on the table

---
+++ src/lib/components/table/Table.story.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import { themes } from '$lib/stores';
+	import Table from './Table.svelte';
+	export let Hst: Hst;
+
+	let theme = 'macchiato';
+</script>
+
+<Hst.Story>
+	<svelte:fragment slot="controls">
+		<Hst.Select title="Theme" bind:value={theme} options={themes} />
+	</svelte:fragment>
+
+	<div class={theme}>
+		<Table
+			header={[
+				{ icon: 'mdi:home', name: 'id', value: 'ID' },
+				{ icon: 'mdi:tea', name: 'test', value: 'Tea' },
+				{ icon: 'mdi:ab-testing', name: 'anything', value: 'Anything' },
+				{ icon: 'mdi:abacus', name: 'foo', value: 'Foo' }
+			]}
+			data={[
+				{ id: '1', test: 'Lorem', anything: 'asdf', foo: 'Foo' },
+				{ id: '2', test: 'Lorem', anything: 'asdf', foo: 'Foo' },
+				{ id: '3', test: 'Lorem', anything: 'asdf', foo: 'Foo' },
+				{ id: '4', test: 'Lorem', anything: 'asdf', foo: 'Foo' },
+				{ id: '5', test: 'Lorem', anything: 'asdf', foo: 'Foo' }
+			]}
+		/>
+	</div>
+</Hst.Story>
---
+++ src/lib/components/table/Table.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" generics="T extends string">
+	import Icon from '@iconify/svelte';
+
+	export let header: { name: T; value: string; icon: string }[];
+
+	export let data: Record<T, string>[];
+</script>
+
+<div class="rounded-lg text-text bg-surface0">
+	<table class="border-separate border-spacing-8">
+		<tr>
+			{#each header as { value, icon }}
+				<th>
+					<div class="flex items-center gap-2 text-xl">
+						<Icon {icon} />
+						<span>
+							{value}
+						</span>
+					</div>
+				</th>
+			{/each}
+		</tr>
+
+		{#each data as row}
+			<tr class="border-2 border-text">
+				{#each header as { name }}
+					<td>
+						<span class="text-lg">
+							{row[name]}
+						</span>
+					</td>
+				{/each}
+			</tr>
+		{/each}
+	</table>
+</div>
