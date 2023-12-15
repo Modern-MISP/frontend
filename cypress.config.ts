624a3418a3602c69018feecaa99839f18c537ca3
Fea <feathecutie@disroot.org>
2023-12-15 22:15:15 +0100

    Made Cypress mostly work, still need to debug tailwind

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'cypress';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
 	component: {
 		devServer: {
 			framework: 'svelte',
-			bundler: 'vite'
+			bundler: 'vite',
+			// Cypress doesn't support the Vite SvelteKit plugin, only Svelte directly.
+			// See https://github.com/cypress-io/cypress/issues/26064#issuecomment-1475437226
+			viteConfig: () => {
+				return {
+					plugins: [svelte({ configFile: false, preprocess: [vitePreprocess()] })]
+				};
+			}
 		}
 	},
 
