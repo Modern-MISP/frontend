ef13aed5d1c4934dbbf369f29cb5e1b05ec929a4
Linus Bartsch <git@libasoft.de>
2024-01-13 00:21:41 +0100

    improve svelteflow setup; remove @catppuccin/palette dependency

--- src/lib/components/svelteflow/Flow.svelte
+++ src/lib/components/svelteflow/Flow.svelte
@@ -1,20 +1,27 @@
 <script lang="ts">
-  import { SvelteFlow, Background, BackgroundVariant, Controls, type Node, type Edge } from '@xyflow/svelte';
+  import {
+    SvelteFlow,
+    Background,
+    BackgroundVariant,
+    Controls,
+    type Node,
+    type Edge
+  } from '@xyflow/svelte';
   import TriggerNode from './nodes/TriggerNode.svelte';
   import type { Writable } from 'svelte/store';
   import ModuleNode from './nodes/ModuleNode.svelte';
 
   /** Nodes that are rendered on the flow */
   export let nodes: Writable<Node[]>;
-  
+
   /** Edges that are rendered on the flow */
   export let edges: Writable<Edge[]>;
 
   const nodeTypes = {
     trigger: TriggerNode,
-    module: ModuleNode,
-  }
-  
+    module: ModuleNode
+  };
+
   /** Dimensions of the grid that nodes will snap onto */
   export let snapGrid: [number, number] = [25, 25];
 </script>
@@ -30,18 +37,14 @@
 -->
 <SvelteFlow {nodes} {edges} {snapGrid} {nodeTypes} fitView on:nodeclick class="text-text">
   <div class="!text-base">
-    <Background
-      class="!bg-base"
-      variant={BackgroundVariant.Dots}
-      size={2} />
-    <Controls position="top-right"></Controls>
-    <slot></slot>
+    <Background class="!bg-base" variant={BackgroundVariant.Dots} size={2} />
+    <Controls position="top-right" />
+    <slot />
   </div>
 </SvelteFlow>
 
 <style lang="postcss">
   :global(:root) {
-      --minimap-background-color-props: theme('colors.current');
+    --minimap-background-color-props: theme('colors.current');
   }
 </style>
-  
--- src/lib/components/svelteflow/nodes/BaseNode.svelte
+++ src/lib/components/svelteflow/nodes/BaseNode.svelte
@@ -13,6 +13,6 @@
   The base component for all custom diagram nodes.
   Other custom node types should use this as their container.
 -->
-<div class="px-4 py-2 shadow-md rounded-md bg-surface1">
-  <slot></slot>
+<div class="px-4 py-2 rounded-md shadow-md bg-surface1">
+  <slot />
 </div>
