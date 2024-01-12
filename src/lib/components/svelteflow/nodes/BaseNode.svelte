839d1ef6363b02cf3865e574f1135bdec6999c6b
Linus Bartsch <git@libasoft.de>
2024-01-13 00:21:41 +0100

    improve svelteflow setup; remove @catppuccin/palette dependency

---
+++ src/lib/components/svelteflow/nodes/BaseNode.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { NodeProps } from '@xyflow/svelte';
+
+  type $$Props = NodeProps;
+
+  /** Node properties */
+  export let data: $$Props['data'];
+</script>
+
+<div class="px-4 py-2 shadow-md rounded-md bg-surface1">
+  <slot></slot>
+</div>
---
+++ src/lib/components/svelteflow/nodes/ModuleNode.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import BaseNode from '$lib/components/svelteflow/nodes/BaseNode.svelte';
+  import { Handle, Position, type NodeProps } from '@xyflow/svelte';
+
+  type $$Props = NodeProps;
+
+  /** Node properties */
+  export let data: $$Props['data'];
+</script>
+
+<BaseNode {...data}>
+  <div class="flex">
+    {data.label}
+  </div>
+  <Handle
+    type="target"
+    position={Position.Left}
+    class="w-16 !bg-teal-500 rounded-none border-none"
+  />
+</BaseNode>
