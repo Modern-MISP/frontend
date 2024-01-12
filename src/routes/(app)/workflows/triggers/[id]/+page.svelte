7defb94b7c04add31ab6b386f256fd65dc2017ac
Linus Bartsch <git@libasoft.de>
2024-01-13 00:21:41 +0100

    improve svelteflow setup; remove @catppuccin/palette dependency

--- src/routes/(app)/workflows/triggers/[id]/+page.svelte
+++ src/routes/(app)/workflows/triggers/[id]/+page.svelte
@@ -1,49 +1,48 @@
 <script lang="ts">
   import DynCard from '$lib/components/card/dynCard/DynCard.svelte';
-  import { writable } from 'svelte/store';
+  import { writable, type Writable } from 'svelte/store';
   import type { PageData } from './$types';
-  import { SvelteFlow, Background, BackgroundVariant, Controls } from '@xyflow/svelte';
-  import { flavors } from '@catppuccin/palette';
-  import { settings } from '$lib/stores';
-    import TriggerNode from './TriggerNode.svelte';
-
-  const colors = flavors[$settings.theme].colors;
+  import type { Node, Edge } from '@xyflow/svelte';
+  import Flow from '$lib/components/svelteflow/Flow.svelte';
 
   export let data: PageData;
 
   const { trigger, infoHeader } = data;
+  const wfData = trigger.Workflow?.data ?? {};
 
-  const nodes = writable([
+  console.log(trigger);
+
+  const nodes: Writable<Node[]> = writable([
     {
       id: '1',
-      type: 'input',
-      data: { label: 'Input Node' },
-      position: { x: 0, y: 0 },
-      class: 'bg-base',
-    },
-    {
-      id: '2',
       type: 'trigger',
-      data: { label: 'Node' },
-      position: { x: 0, y: 150 }
-    }
-  ]);
-  
-  const edges = writable([
-    {
-      id: '1-2',
-      type: 'default',
-      source: '1',
-      target: '2',
-      label: 'Edge Text'
+      data: { label: wfData[1].data.name },
+      position: { x: wfData[1].pos_x, y: wfData[1].pos_y }
     }
   ]);
 
-  const nodeTypes = {
-    trigger: TriggerNode,
+  const edges: Writable<Edge[]> = writable([]);
+
+  for (let i = 2; ; i++) {
+    const module = wfData[i];
+    if (!module) {
+      break;
+    }
+    $nodes.push({
+      id: `${i}`,
+      type: 'module',
+      data: { label: module.name },
+      position: { x: module.pos_x, y: module.pos_y },
+      class: '!bg-surface1',
+    });
+    $edges.push({
+      id: `1-${i}`,
+      type: 'default',
+      source: '1',
+      target: `${i}`,
+      label: 'triggers'
+    })
   }
- 
-  const snapGrid = [25, 25];
 </script>
 
 <div class="flex flex-row">
@@ -51,14 +50,6 @@
     <DynCard header={infoHeader} data={trigger} />
   </div>
   <div class="flex-col w-full basis-2/3">
-    <SvelteFlow {nodes} {edges} {snapGrid} {nodeTypes}>
-      <Background
-        bgColor={colors.crust.hex}
-        patternColor={colors.overlay0.hex}
-        variant={BackgroundVariant.Dots}
-        size={2} />
-      <Controls position="top-right"></Controls>
-    </SvelteFlow>
+    <Flow {nodes} {edges}></Flow>
   </div>
 </div>
-  
