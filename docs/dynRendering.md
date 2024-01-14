577753ff0967aa63f10eacf51a966623550caac8
Lucas Wiese <lucas.wiese@gmx.de>
2024-01-14 16:11:29 +0100

    added docs for error and layout. Also added template files for overview

---
+++ docs/dynRendering.md
@@ -0,0 +1,3 @@
+# Dynamic Layout generation
+
+## createTableHeadGenerator
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,9 @@
 <!--
   @component
   
-  Root layout.
+  Root layout. Used to apply the theme and render the full application. 
+  The theme works with css variables and tailwind. Basically you can apply ever class from  
+{@link https://github.com/catppuccin/tailwindcss} to any element, and the theme will change the colors with css variables.
   
 -->
 <body class="{$settings.theme} bg-base w-full h-[100vh] flex">
