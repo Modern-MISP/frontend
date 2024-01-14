9728618eceb8f3d83272361e118801ba9f0dec34
Linus Bartsch <git@libasoft.de>
2024-01-14 12:03:51 +0100

    add component docs

--- src/lib/components/svelteflow/nodes/BaseNode.svelte
+++ src/lib/components/svelteflow/nodes/BaseNode.svelte
@@ -7,6 +7,12 @@
   export let data: $$Props['data'];
 </script>
 
+<!--
+  @component
+  
+  The base component for all custom diagram nodes.
+  Other custom node types should use this as their container.
+-->
 <div class="px-4 py-2 shadow-md rounded-md bg-surface1">
   <slot></slot>
 </div>
--- src/routes/(app)/workflows/triggers/+page.svelte
+++ src/routes/(app)/workflows/triggers/+page.svelte
@@ -2,9 +2,15 @@
   import DynTable from '$lib/components/table/dynTable/DynTable.svelte';
   import type { PageData } from './$types';
 
+  /** The data that will be displayed on this page. */
   export let data: PageData;
 
   const { tableData, header } = data;
 </script>
 
+<!--
+  @component
+  
+  A list of all workflow triggers.
+-->
 <DynTable href={({ id }) => `/workflows/triggers/${id}`} {header} data={tableData} />
