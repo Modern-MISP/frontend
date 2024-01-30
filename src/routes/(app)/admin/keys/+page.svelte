ae5ad107cdf5fb5136914734b86bc47195e34e31
Lucas Wiese <lucas.wiese@gmx.de>
2024-01-30 17:20:11 +0100

    admin route list actionBars

--- src/routes/(app)/admin/keys/+page.svelte
+++ src/routes/(app)/admin/keys/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+  import { actionBar } from '$lib/actions';
   import DynTable from '$lib/components/table/dynTable/DynTable.svelte';
+  import type { ActionBarEntryProps } from '$lib/models/ActionBarEntry.interface.js';
 
   export let data;
 
   const { tableData, header } = data;
+  const actions: ActionBarEntryProps[] = [
+    {
+      icon: 'mdi:key-add',
+      label: 'Add Key',
+      action: '/admin/keys/new'
+    }
+  ];
 </script>
 
 <!--
@@ -12,4 +21,5 @@
   
 -->
 
+<svelte:window use:actionBar={actions} />
 <DynTable {header} data={tableData} href={(x) => `/admin/keys/${x.AuthKey?.id}`} />h
