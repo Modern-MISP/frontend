438e1ec0539e6587e4d84ed93903e5a7b2a233c2
kaip <ixaster4@gmail.com>
2024-01-30 17:19:30 +0100

    view remote server [id]

--- src/routes/(app)/admin/servers/[id]/+page.svelte
+++ src/routes/(app)/admin/servers/[id]/+page.svelte
@@ -1,5 +1,18 @@
+<script lang="ts">
+    import DynCard from '$lib/components/card/dynCard/DynCard.svelte';
+    import type { PageData } from './$types';
+  
+    /** Data that is displayed on this page. */
+    export let data: PageData;
+  
+    const { left, right, server } = data;
+ </script>
 <!--
     @component
     Displays information about a specific remote server, specified by `id`.
     
 -->
+<div class="flex flex-wrap w-full gap-2 lg:flex-nowrap">
+    <DynCard header={left} data={server} />
+    <DynCard header={right} data={server} />
+</div>
