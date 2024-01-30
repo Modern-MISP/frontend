0407adee8f6b24ee710d029297a38537a4800299
Lucas Wiese <lucas.wiese@gmx.de>
2024-01-30 17:52:50 +0100

    fixed default redirects on "/". Added href to home icon. Removed user from top menu

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,6 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
   const token = localStorage.getItem('authToken');
-  if (!token) redirect(307, '/login');
-  redirect(307, '/events');
+  if (!token) throw redirect(307, '/login');
+
+  throw redirect(307, '/events');
 };
