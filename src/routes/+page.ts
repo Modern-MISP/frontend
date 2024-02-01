ee3a1c791d983f231e57b1c0ec7162a5c6e1d67a
Fea Sattler <ubtiw@student.kit.edu>
2024-02-01 15:44:51 +0000

    removed throw again

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
   const token = localStorage.getItem('authToken');
-  if (!token) throw redirect(307, '/login');
+  if (!token) redirect(307, '/login');
 
   throw redirect(307, '/events');
 };
