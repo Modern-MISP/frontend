225040ed05776b1306f367eac3e7f02032860f69
Lucas Wiese <lucas.wiese@gmx.de>
2024-02-07 17:08:44 +0100

    implemented typeahead search for filter

--- src/routes/(app)/tags/+page.svelte
+++ src/routes/(app)/tags/+page.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
+  import { actionBar } from '$lib/actions.js';
+  import Input from '$lib/components/input/Input.svelte';
   import Pagination from '$lib/components/pagination/Pagination.svelte';
+  import ActionCard from '$lib/components/table/actions/card/ActionCard.svelte';
   import DynTable from '$lib/components/table/dynTable/DynTable.svelte';
-  import type { PageData } from './$types';
 
   /**
    * Page data
    */
-  export let data: PageData;
+  export let data;
 
-  let { tableData, header } = data;
+  $: ({ tableData, header } = data);
+
+  let tagFilter = '';
+
+  $: filtered = tableData.filter((x) => x.name?.includes(tagFilter));
 
   // FIXME: should be implemented with paginated post request to api. Server fault...
-  $: sliced = tableData.slice(50 * page - 50, 50 * page);
+  $: sliced = filtered.slice(50 * page - 50, 50 * page);
   $: page = 1;
 </script>
 
@@ -20,6 +26,14 @@
   Displays a combined list of the tags of all events.
   
 -->
+<svelte:window use:actionBar={[]} />
+
+<div class="flex flex-row gap-1">
+  <ActionCard>
+    <Input placeholder="Search tag" class="w-max" on:value={({ detail }) => (tagFilter = detail)}
+    ></Input>
+  </ActionCard>
+</div>
 
 <DynTable href={({ id }) => `tags/${id}`} {header} data={sliced} />
 
