894721a5b02441c58ab0b48f9b14389ed58fc032
Lucas Wiese <lucas.wiese@gmx.de>
2024-02-08 14:07:38 +0100

    galaxies uses ComplexTableLayout

--- src/routes/(app)/galaxies/+page.svelte
+++ src/routes/(app)/galaxies/+page.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
   import EnableFilter from './EnableFilter.svelte';
 
-  import { actionBar } from '$lib/actions';
-  import DynActionCard from '$lib/components/table/actions/dynCard/DynActionCard.svelte';
-  import SelectionCard from '$lib/components/table/actions/selectionCard/SelectionCard.svelte';
-  import DynTable from '$lib/components/table/dynTable/DynTable.svelte';
-
-  import { mode } from '$lib/stores';
+  import ComplexTableLayout from '$lib/components/table/complexTable/ComplexTableLayout.svelte';
 
   /**
    * The data that will be displayed on this page
    */
   export let data;
 
-  $: ({ tableData, header, editActions, topMenuActions } = data);
-
-  let activeRows: typeof tableData = [];
+  $: ({ tableData } = data);
 
   let filtered: typeof tableData = [];
 </script>
@@ -26,26 +19,8 @@
   A list of all galaxies.
 -->
 
-<svelte:window use:actionBar={topMenuActions} />
-
-<div class="flex flex-row gap-1">
-  <EnableFilter bind:data={tableData} bind:filtered></EnableFilter>
-
-  {#if $mode === 'edit'}
-    <SelectionCard
-      numSelected={activeRows.length}
-      selectAll={() => (activeRows = tableData)}
-      unselectAll={() => (activeRows = [])}
-    />
-
-    <DynActionCard header={editActions} data={activeRows}></DynActionCard>
-  {/if}
-</div>
-
-<DynTable
-  href={(x) => `/galaxies/${x.id}`}
-  {header}
-  data={filtered}
-  bind:activeRows
-  selectMode={$mode === 'edit'}
-/>
+<ComplexTableLayout {...data} tableData={filtered} filter={[]} pagination={false}>
+  <div slot="filter">
+    <EnableFilter bind:data={tableData} bind:filtered></EnableFilter>
+  </div>
+</ComplexTableLayout>
