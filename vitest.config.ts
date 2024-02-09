e949c2192f8ba220c3b7d555c5962e72fa857e43
Fea <feathecutie@disroot.org>
2024-02-09 14:18:40 +0100

    Add vitest code coverage

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    reporters: ['junit'],
+    outputFile: 'report.xml',
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['cobertura']
+    }
+  }
+});
