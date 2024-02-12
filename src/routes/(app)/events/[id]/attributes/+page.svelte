88aa077e2d2ab8312cc75e2577ab4c84d615134f
Linus Bartsch <git@libasoft.de>
2024-02-12 18:29:58 +0100

    add action bar entry for add event attribute

--- src/routes/(app)/events/[id]/attributes/+page.svelte
+++ src/routes/(app)/events/[id]/attributes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import ComplexTableLayout from '$lib/components/table/complexTable/ComplexTableLayout.svelte';
   import type { DynTableHeadExtent } from '$lib/components/table/dynTable/DynTable.model';
+  import type { ActionBarEntryProps } from '$lib/models/ActionBarEntry.interface';
   import { createTableHeadGenerator } from '$lib/util/tableBuilder.util';
   import tableHeader from '../../../attributes/attributeCols';
 
@@ -20,6 +21,15 @@
     col(tableHeader.type),
     col(tableHeader.deleted)
   ];
+
+  const topMenuActions: ActionBarEntryProps[] = [
+    {
+      icon: 'mdi:plus',
+      label: 'Add',
+      action: `/events/${data.event.id}/attributes/new`
+    }
+  ];
 </script>
 
-<ComplexTableLayout {tableData} {header} editActions={[]} filter={[]}></ComplexTableLayout>
+<ComplexTableLayout {tableData} {header} editActions={[]} filter={[]} {topMenuActions}
+></ComplexTableLayout>
