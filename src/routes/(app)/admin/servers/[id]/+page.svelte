257e6202ea1642ee7d03477218d4078299789284
Linus Bartsch <git@libasoft.de>
2024-02-13 19:23:51 +0100

    add form wrappers for admin routes and fix them (partially)

--- src/routes/(app)/admin/servers/[id]/+page.svelte
+++ src/routes/(app)/admin/servers/[id]/+page.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
+  import { page } from '$app/stores';
+  import { api } from '$lib/api';
   import DynCard from '$lib/components/card/dynCard/DynCard.svelte';
+  import Form from '$lib/components/form/Form.svelte';
+  import { notifySave } from '$lib/util/notifications.util';
   import type { PageData } from './$types';
 
   /** Data that is displayed on this page. */
   export let data: PageData;
 
   const { left, right, server } = data;
+
+  function editCallback(formData: Record<string, string>) {
+    // FIXME: error 500 without further info
+    notifySave(
+      $api
+        .PUT('/servers/edit/{serverId}', {
+          params: { path: { serverId: $page.params.id } },
+          body: formData
+        })
+        .then((resp) => {
+          if (resp.error) throw new Error(resp.error.message);
+        })
+    );
+  }
 </script>
 
 <!--
@@ -13,7 +31,9 @@
     Displays information about a specific remote server, specified by `id`.
     
 -->
-<div class="flex flex-wrap w-full gap-2 lg:flex-nowrap">
-  <DynCard header={left} data={server} />
-  <DynCard header={right} data={server} />
-</div>
+<Form callback={editCallback}>
+  <div class="flex flex-wrap w-full gap-2 lg:flex-nowrap">
+    <DynCard header={left} data={server} />
+    <DynCard header={right} data={server} />
+  </div>
+</Form>
