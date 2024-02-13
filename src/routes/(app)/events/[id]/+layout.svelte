41623bffab164a9a27ddac1a7bd5a3b11d5669a4
Fea <feathecutie@disroot.org>
2024-02-13 18:03:52 +0100

    Make events context menu not disappear

---
+++ src/routes/(app)/events/[id]/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { page } from '$app/stores';
+  import { contextRoutes } from '$lib/actions';
+</script>
+
+<svelte:window
+  use:contextRoutes={[
+    {
+      name: 'Event Info',
+      icon: 'mdi:information-outline',
+      href: `/events/${$page.params.id}/#`
+    },
+    {
+      name: 'Event Galaxies',
+      icon: 'streamline:galaxy-2-solid',
+      href: `/events/${$page.params.id}/galaxies`
+    },
+    {
+      name: 'Event Attributes',
+      icon: 'mdi:flag',
+      href: `/events/${$page.params.id}/attributes`
+    },
+    {
+      name: 'Event Graph',
+      icon: 'ph:graph',
+      href: `/events/${$page.params.id}/graph`
+    }
+  ]}
+/>
+
+<slot />
