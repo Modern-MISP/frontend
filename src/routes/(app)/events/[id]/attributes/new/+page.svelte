ac0285f4f9d15e9669e765b22ecfc5db1f414794
Linus Bartsch <git@libasoft.de>
2024-02-13 14:29:09 +0100

    add save action entry

--- src/routes/(app)/events/[id]/attributes/new/+page.svelte
+++ src/routes/(app)/events/[id]/attributes/new/+page.svelte
@@ -3,6 +3,8 @@
   import { createTableHeadGenerator } from '$lib/util/tableBuilder.util';
   import { page } from '$app/stores';
   import DynCard from '$lib/components/card/dynCard/DynCard.svelte';
+  import { actionBar } from '$lib/actions';
+  import type { ActionBarEntryProps } from '$lib/models/ActionBarEntry.interface';
 
   export let data;
 
@@ -18,9 +20,19 @@
     col(attributeCols.event),
     col(attributeCols.distribution, editCols.distribution),
     col(attributeCols.category, editCols.category),
+    col(attributeCols.type, editCols.type),
     col(attributeCols.comment, editCols.comment),
     col(attributeCols.value, editCols.value)
   ];
+
+  const actions: ActionBarEntryProps[] = [
+    {
+      icon: 'material-symbols:save-outline',
+      label: 'Save',
+      action: () => {}
+    }
+  ];
 </script>
 
+<svelte:window use:actionBar={actions} />
 <DynCard {header} data={attributeData} />
