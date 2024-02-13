8c119c8fd8f2945a39d549bec7609a5e090581de
Linus Bartsch <git@libasoft.de>
2024-02-13 10:07:44 +0100

    start working on add attribute

---
+++ src/routes/(app)/events/[id]/attributes/new/+page.ts
@@ -0,0 +1,14 @@
+import { api } from '$lib/api';
+import { error, type NumericRange } from '@sveltejs/kit';
+import { get } from 'svelte/store';
+
+export async function load() {
+  const { error: mispError, data, response } = await get(api).GET('/attributes/describeTypes');
+
+  if (mispError) throw error(response.status as NumericRange<400, 599>, mispError.message);
+
+  return {
+    // cast type to fix wrong MISP API spec
+    attributeTypes: (data as unknown as { result: typeof data }).result
+  };
+}
