ce125173aa38920e47fd20d6e8c6fcc1854e13bd
Fea <feathecutie@disroot.org>
2024-02-13 15:21:57 +0100

    Get event graph references

--- src/routes/(app)/events/[id]/+layout.ts
+++ src/routes/(app)/events/[id]/+layout.ts
@@ -4,16 +4,50 @@ import { error, type NumericRange } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 
 export const load = async ({ params, fetch }) => {
+  const { GET } = get(api);
+
   const {
     data,
     error: mispError,
     response
-  } = await get(api).GET('/events/view/{eventId}', {
+  } = await GET('/events/view/{eventId}', {
     params: { path: { eventId: params.id } },
     fetch
   });
 
   if (mispError) error(response.status as NumericRange<400, 599>, mispError.message);
+
+  // @ts-expect-error Not in the OpenAPI spec
+  const eventGraphReferencesResult = GET('/events/getEventGraphReferences/{eventId}', {
+    params: { path: { eventId: params.id } }
+  });
+
+  // NOTE: This type is handwritten and mostly guessed based on the API response, it might be wrong, use with caution.
+  const eventGraphReferences = (await eventGraphReferencesResult).data as {
+    available_pivot_key: string[];
+    existing_object_relation: Record<string, number>;
+    existing_tags: Record<number, string>;
+    items: {
+      id: string;
+      label: string;
+      node_type: string;
+      type: string;
+      uuid: string;
+      comment: string;
+      event_id: string;
+    }[];
+    relations: {
+      comment: string;
+      event_id: string;
+      from: string;
+      id: string;
+      to: string;
+      type: string;
+      uuid: string;
+    }[];
+  };
+
+  console.log(eventGraphReferences);
 
   return {
     event: data.Event! as NonNullable<typeof data.Event> & {
