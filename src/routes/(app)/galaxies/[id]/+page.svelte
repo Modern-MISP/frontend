d60982f720ea01d9ab599a77af9137cf4939aff6
Fea <feathecutie@disroot.org>
2024-02-14 18:05:52 +0100

    Add button for adding new galaxy cluster

--- src/routes/(app)/galaxies/[id]/+page.svelte
+++ src/routes/(app)/galaxies/[id]/+page.svelte
@@ -2,6 +2,8 @@
   import DynTable from '$lib/components/table/dynTable/DynTable.svelte';
   import DynCard from '$lib/components/card/dynCard/DynCard.svelte';
   import type { PageData } from './$types';
+  import { actionBar } from '$lib/actions';
+  import { page } from '$app/stores';
 
   /** Data that is displayed on this page. */
   export let data: PageData;
@@ -16,10 +18,21 @@
   
   Information about a single galaxy, including a list of its clusters.
 -->
+
 <div class="flex flex-wrap w-full gap-2 lg:flex-nowrap">
   <DynCard header={left} data={galaxyData} />
   <DynCard header={right} data={galaxyData} />
 </div>
+
+<svelte:window
+  use:actionBar={[
+    {
+      icon: 'mdi:plus',
+      label: 'New Galaxy Cluster',
+      action: `/galaxies/${$page.params.id}/new_cluster`
+    }
+  ]}
+/>
 
 <DynTable
   href={({ id }) => `/galaxies/clusters/${id}`}
