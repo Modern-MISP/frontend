a41410ec89ffc6e025a9f116a3d79ff1fd9fcb5b
Linus Bartsch <git@libasoft.de>
2024-02-16 22:50:47 +0100

    add site metadata title, description and more

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,13 @@
   Elements using the proper tailwind classes will be themed automatically according to the current theme when placed in this layout.
   
 -->
+<svelte:head>
+  <title>MMISP</title>
+  <meta name="application-name" content="MMISP" />
+  <meta name="description" content="Modern MISP Frontend" />
+  <meta name="keywords" content="MISP" />
+  <meta charset="utf-8" />
+</svelte:head>
 <body id="layout" class="{$settings.theme} bg-base w-full h-[100vh] flex">
   <slot />
 </body>
