b67117c9a5bb036072519ec3afbef8dcd9034fa6
Linus Bartsch <git@libasoft.de>
2024-02-16 18:28:07 +0100

    start implementing create tag

--- src/routes/(app)/tags/CreateTag.svelte
+++ src/routes/(app)/tags/CreateTag.svelte
@@ -1,7 +1,25 @@
-<script>
-  import Card from '$lib/components/card/Card.svelte';
+<script lang="ts">
+  import { api } from '$lib/api';
+  import Form from '$lib/components/form/Form.svelte';
+  import CreateTagForm from '$lib/components/tagForms/CreateTagForm.svelte';
+  import { get } from 'svelte/store';
+  import { notifySave } from '$lib/util/notifications.util';
+
+  function addTag(formData: Record<string, string>) {
+    notifySave(
+      get(api)
+        .POST('/tags/add', {
+          body: formData
+        })
+        .then((res) => {
+          if (res.error) throw Error(res.error.message);
+        })
+    );
+  }
 </script>
 
-<div class="absolute left-0 z-50 w-1/2 h-full">
-  <Card class="bg-surface2">Create Tag...</Card>
+<div class="absolute left-0 z-50 w-1/2 h-full overflow-auto">
+  <Form callback={addTag}>
+    <CreateTagForm />
+  </Form>
 </div>
