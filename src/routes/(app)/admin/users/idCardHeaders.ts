f3578107ba06649e6392ea886abe5fdf8eca37fd
Fea <feathecutie@disroot.org>
2024-02-16 16:18:48 +0100

    Fix type error

--- src/routes/(app)/admin/users/idCardHeaders.ts
+++ src/routes/(app)/admin/users/idCardHeaders.ts
@@ -1,9 +1,10 @@
-import type { paths } from '$lib/api/misp';
+import type { components, paths } from '$lib/api/misp';
 import { createTableHeadGenerator } from '$lib/util/tableBuilder.util';
 import Checkbox from '$lib/components/checkbox/Checkbox.svelte';
 import Boolean from '$lib/components/boolean/Boolean.svelte';
 import Input from '$lib/components/input/Input.svelte';
 import DatePill from '$lib/components/pills/datePill/DatePill.svelte';
+import Pill from '$lib/components/pills/pill/Pill.svelte';
 import Info from '$lib/components/info/Info.svelte';
 
 const col = createTableHeadGenerator<
@@ -14,6 +15,7 @@ const col = createTableHeadGenerator<
       notification_monthly?: boolean;
       last_pw_change?: number;
     };
+    Organisation?: components['schemas']['Organisation'];
   }
 >();
 
@@ -179,6 +181,12 @@ export default {
   organisation: col({
     key: 'org',
     label: 'Organizations',
-    value: (x) => ({ display: Info, props: { text: x.Organisation?.name ?? 'unknown' } })
+    value: (x) => ({
+      display: Pill,
+      props: {
+        text: x?.Organisation?.name,
+        icon: 'material-symbols:work-outline'
+      }
+    })
   })
 };
