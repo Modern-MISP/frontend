6ad9084bd11858ff0fd87de793358a19bed3eca4
Fea <feathecutie@disroot.org>
2024-02-16 22:37:18 +0100

    Try to fix typescript stuff

--- src/routes/(app)/events/[id]/attributes/new/+page.ts
+++ src/routes/(app)/events/[id]/attributes/new/+page.ts
@@ -1,9 +1,10 @@
 import { api } from '$lib/api';
+// import { api } from '../../../../../../lib/api';
 import { error, type NumericRange } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 
 export async function load() {
-  const { error: mispError, data, response } = await get(api).GET('/attributes/describeTypes');
+  const { error: mispError, data, response } = await get(api).GET('/attributes/describeTypes', {});
 
   if (mispError) throw error(response.status as NumericRange<400, 599>, mispError.message);
 
