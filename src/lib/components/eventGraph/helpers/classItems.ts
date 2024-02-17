db9312c0c55b10f2cd969643e31d159e4f6eeb58
uyhad <uyhad@student.kit.edu>
2024-02-17 10:20:37 +0100

    fixed no-explicit-any issue

--- src/lib/components/eventGraph/helpers/classItems.ts
+++ src/lib/components/eventGraph/helpers/classItems.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export function getReferencedItems(objects: any[], attributes: any[], references: any[]) {
   const referencedItemsIds = extractReferencedItemsIds(references);
   const referencedObjectsIds = filterReferencedObjectsIds(referencedItemsIds);
