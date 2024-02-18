007223f0a1a8e7dedc5d8c08947e948abaf33adb
Fea <feathecutie@disroot.org>
2024-02-18 20:56:52 +0100

    Lock tags view into view mode

--- src/routes/(app)/tags/[id]/+page.svelte
+++ src/routes/(app)/tags/[id]/+page.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
+  import { lockViewMode } from '$lib/actions';
   import DynCard from '$lib/components/card/dynCard/DynCard.svelte';
+    import { mode } from '$lib/stores';
   import type { PageData } from './$types';
 
   /** Data that is displayed on this page. */
   export let data: PageData;
 
   const { header, user } = data;
+
+  $mode = 'view';
 </script>
 
 <!--
     @component
     Shows information about a specific tag, specified by `id`.
 -->
+
+<svelte:window use:lockViewMode={true} />
+
 <div class="flex flex-wrap w-full gap-2 lg:flex-nowrap">
   <DynCard {header} data={user} />
 </div>
