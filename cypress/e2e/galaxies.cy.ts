9f172c4d4e63410d6126d6d57e97856caa7d8385
Linus Bartsch <git@libasoft.de>
2024-03-01 13:56:02 +0100

    remove unneccessary wait

--- cypress/e2e/galaxies.cy.ts
+++ cypress/e2e/galaxies.cy.ts
@@ -6,7 +6,6 @@ describe('galaxies', () => {
   it('should be reachable via side menu', () => {
     cy.visit('/events');
     cy.get('aside > nav').find('a[href="/galaxies"]').click();
-    cy.wait(100);
     cy.url().should('include', '/galaxies');
   });
 });
