8969319fe416d9abc8026119cd2efc85f07832f2
Linus Bartsch <git@libasoft.de>
2024-03-06 17:02:33 +0100

    add test case for /T010/

---
+++ cypress/e2e/tags.cy.ts
@@ -0,0 +1,18 @@
+beforeEach(() => {
+  cy.defaultLogin();
+})
+
+describe('tags', () => {
+  it('should be creatable', () => {
+    cy.visit('/tags');
+    cy.toggleMode();
+    cy.get('#actionBar > :contains("Create Tag")').click();
+    cy.get('input[name="name"]').type('test tag');
+    cy.get('select[name="org_id"]').select('1');
+    cy.get(':has(> input[name="hide_tag"])').click();
+    cy.get(':has(> input[name="local_only"])').click();
+    cy.get('button[type="submit"]').click()  // save
+
+    // TODO: check if created
+  });
+});
