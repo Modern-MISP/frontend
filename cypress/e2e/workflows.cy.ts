bc2bd50a0f542a93b90f49b93f061d8875c80190
Linus Bartsch <git@libasoft.de>
2024-03-11 16:13:45 +0100

    add test case for add workflow connection

--- cypress/e2e/workflows.cy.ts
+++ cypress/e2e/workflows.cy.ts
@@ -17,4 +17,14 @@ describe('workflows', () => {
 
     cy.get('.svelte-flow__node:contains("Attach warninglist")').should('exist');
   });
+
+  it('can add connection', () => {
+    cy.visit('/workflows/1');
+    cy.toggleMode();
+    cy.get('#actionBar > button:contains("Save")').should('be.disabled');
+    const sourceHandle = cy.get('.svelte-flow__node-trigger .svelte-flow__handle');
+    sourceHandle.drag('.svelte-flow__node:contains("Enrich+Event") .svelte-flow__handle-left');
+    cy.get('#actionBar > button:contains("Save")').should('not.be.disabled');
+    cy.get('.svelte-flow__edge[data-id="xy-edge__1output_1-4input_1"]').should('exist');
+  });
 });
