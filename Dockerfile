8e23441bc493de164a116713bff151fb6de7552c
Fea <feathecutie@disroot.org>
2024-03-11 01:06:02 +0100

    Basic setup for automated e2e tests

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM node:19-alpine3.16 AS builder
 
+
+ARG PUBLIC_MAINTENANCE_MODE
+ARG PUBLIC_MAINTENANCE_MESSAGE
+ARG PUBLIC_REST_DISABLED
+ARG PUBLIC_REST_DISABLED_MESSAGE
+ARG PUBLIC_MISP_API_ENDPOINT
+ARG PUBLIC_DATE_FORMAT
+
 WORKDIR /app
 COPY package*.json .
 RUN npm ci
@@ -8,5 +16,5 @@ RUN npm run build
 
 
 FROM nginx:latest
-
+COPY nginx.conf /etc/nginx/nginx.conf
 COPY --from=builder /app/build /usr/share/nginx/html/
--- set_misp_settings.sh
+++ set_misp_settings.sh
@@ -1,4 +1,4 @@
 # This should be executed after MISP is running, this is just a temporary file to not lose the commands
-docker exec misp-docker-misp-core-1 app/Console/cake Admin setSetting Security.allow_cors true
-docker exec misp-docker-misp-core-1 app/Console/cake Admin setSetting Security.cors_origins '*'
-docker exec misp-docker-misp-core-1 app/Console/cake Admin setSetting Plugin.workflow_enable true
+docker compose exec misp-core app/Console/cake Admin setSetting Security.allow_cors true
+docker compose exec misp-core app/Console/cake Admin setSetting Security.cors_origins '*'
+docker compose exec misp-core app/Console/cake Admin setSetting Plugin.Workflow_enable true
