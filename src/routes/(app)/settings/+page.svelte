c19f135e96f06ecfec81759da7b292efabb3ad50
khins <kath.hinschuetz@gmail.com>
2024-06-05 17:21:04 +0200

    added export to settings

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
   import { routes } from '../routes';
   import SideMenuEntry from '$lib/components/menus/sidemenu/SideMenuEntry.svelte';
+  import { actionBar } from '$lib/actions';
+  import type { ActionBarEntryProps } from '$lib/models/ActionBarEntry.interface';
 
   const childRoutes = routes.find(({ href }) => href === '/settings')?.children ?? [];
+
+  const topMenuActions: ActionBarEntryProps[] = [
+    {
+      icon: 'mdi:download',
+      label: 'Export',
+      action: () => {
+        console.log('YAY');
+      }
+    }
+  ];
 </script>
+
+<svelte:window use:actionBar={topMenuActions} />
 
 {#each childRoutes as childRoute}
   <SideMenuEntry {...childRoute} isMenuOpen={true}></SideMenuEntry>
