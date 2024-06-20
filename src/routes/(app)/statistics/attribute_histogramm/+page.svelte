6c2923791557a4331bfa7ab8aebed4a832f853d2
uhuqu <uhuqu@student.kit.edu>
2024-06-20 17:43:08 +0200

    Attribute Histogramm

---
+++ src/routes/(app)/statistics/attribute_histogramm/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { lockViewMode } from '$lib/actions';
+  import { mode } from '$lib/stores';
+  import Histogram from '$lib/components/histogram/Histogram.svelte';
+  $mode = 'view';
+
+  /**
+   * Page data containing the activity heatmap.
+   */
+  export let data;
+
+  $: ({ histogramData, dataTypes } = data);
+</script>
+
+<svelte:window use:lockViewMode={true} />
+
+<Histogram title="Histogram" description="This is a histogram" data={histogramData} {dataTypes} />
