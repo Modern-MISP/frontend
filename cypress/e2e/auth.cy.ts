df94190d89c79b716240cdaa6f6cc4807e1235c0
uhuqu <uhuqu@student.kit.edu>
2024-07-27 17:52:41 +0200

    feat: auth tests T002

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -64,10 +64,81 @@ describe('login password', () => {
   });
 });
 
-describe('add tag to event', () => {
-  beforeEach(() => {
+const password = 'password';
+const email2 = 'cypress-test@example.com';
+const user_name2 = 'Cypress Test';
+
+const cardRowRight2 = (text: string) => `div:has(> span:contains("${text}")) > :last-child`;
+
+describe('change password', () => {
+  before(() => {
     cy.defaultLogin();
-    cy.visit('/events/2/galaxies');
+    cy.visit('/admin/users/new');
+
+    cy.get(cardRowRight2('Name')).type(user_name2);
+    cy.get(cardRowRight2('Email')).type(email2);
+    cy.get(cardRowRight2('Password')).clear().type(password);
+    cy.get(cardRowRight2('Contact enables')).click();
+    cy.get(cardRowRight2('Weekly notifications')).click();
+    cy.get(cardRowRight2('Terms accepted')).click();
+
+    cy.get('button:has(span:contains("Save"))').click();
+
+    cy.url().should('match', /\/admin\/users\/\d+/);
+
+    cy.clearAllSessionStorage();
+  });
+
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.get('input[name="email"]').type(email2);
+    cy.get('input[name="password"]').type(password);
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('include', '/login/setPassword');
+  });
+
+  after(() => {
+    cy.clearAllSessionStorage();
+    cy.defaultLogin();
+    cy.visit('/admin/users');
     cy.toggleMode();
+
+    cy.get('tbody > tr:last-child').click();
+    cy.url().should('match', /\/admin\/users\/\d+/);
+    cy.get('button:has(span:contains("Delete"))').click();
+  });
+
+  it('should not allow to set the unsafe password', () => {
+    const stub = cy.stub();
+    cy.on('window:alert', stub);
+
+    cy.get('input[name="old-password"]').type(password);
+    cy.get('input[name="password"]').type('password123');
+    cy.wait(1000);
+    cy.get('input[name="password-repeat"]').type('password123', { force: true });
+    cy.wait(1000);
+    cy.get('button[type="submit"]')
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(
+          'Your password was exposed by a data breach on the Internet.'
+        );
+      });
+
+    cy.url().should('include', '/login/setPassword');
+  });
+
+  it('should allow to set the safe password', () => {
+    cy.get('input[name="old-password"]').type(password);
+    cy.get('input[name="password"]').type('DiesesWortIstSoLangDamitIchsMirGutMerkenKann');
+    cy.wait(1000);
+    cy.get('input[name="password-repeat"]').type('DiesesWortIstSoLangDamitIchsMirGutMerkenKann', {
+      force: true
+    });
+    cy.wait(1000);
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('include', '/events');
   });
 });
