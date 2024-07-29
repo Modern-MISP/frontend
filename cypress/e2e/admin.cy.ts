5002b7b537a1c05c13bafe86e41fef2ad7cfa05b
uhuqu <uhuqu@student.kit.edu>
2024-07-29 18:02:58 +0200

    fix: prettier

--- cypress/e2e/admin.cy.ts
+++ cypress/e2e/admin.cy.ts
@@ -1,12 +1,11 @@
 beforeEach(() => {
-    cy.defaultLogin();
-  });
+  cy.defaultLogin();
+});
 
 describe('Admin Tests', () => {
   it('should be able to navigate to the auth key page', () => {
-      cy.visit('/admin/keys');
+    cy.visit('/admin/keys');
 
-      cy.url().should('include', '/admin/keys');
+    cy.url().should('include', '/admin/keys');
+  });
 });
-});
-
