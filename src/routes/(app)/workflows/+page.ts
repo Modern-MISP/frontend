52b4787586e64e93847d1dbab7196eb3aee737be
ulpov <ulpov@student.kit.edu>
2024-07-31 18:34:42 +0200

    fixed the api link in the workflows page

--- src/routes/(app)/workflows/+page.ts
+++ src/routes/(app)/workflows/+page.ts
@@ -127,7 +127,7 @@ export const load = async ({ fetch }) => {
         Promise.all(
           x.map((workflow) =>
             // @ts-expect-error Not in the OpenAPI spec?
-            get(api).POST('/workflows/toggleDebugField/{workflowId}/{enabled}', {
+            get(api).POST('/workflows/debugToggleField/{workflowId}/{enabled}', {
               fetch,
               params: { path: { workflowId: workflow.id!, enabled: !workflow.debug_enabled } }
             })
