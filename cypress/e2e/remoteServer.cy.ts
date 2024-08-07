3689a6cbc7147735234c146f2fbb33f16c124556
uhuqu <uhuqu@student.kit.edu>
2024-08-07 18:12:06 +0200

    fix: remoteServers cypress

--- cypress/e2e/remoteServer.cy.ts
+++ cypress/e2e/remoteServer.cy.ts
@@ -5,12 +5,14 @@ beforeEach(() => {
 describe('open Remote Server page', () => {
   it('should be able to navigate to the Remote Server page', () => {
     cy.visit('/admin');
-    cy.get('a:contains("Remote Servers")').should('exist').click();
+    cy.get('a:contains("Remote Server")').should('exist').click();
   });
 });
 
 describe('Remote Server add Server', () => {
   it('should be able to navigate to the "new server" page', () => {
+    cy.visit('/admin/servers');
+    
     cy.toggleMode();
 
     cy.get('a:contains("Add Server")').should('exist').click();
