5984f4648eb2492e21781d64d0010d29d94f1099
felix <uhvuh@student.kit.edu>
2024-08-07 05:09:45 +0200

    workflowSettings test

---
+++ cypress/e2e/workflowSettings.cy.ts
@@ -0,0 +1,11 @@
+before(() => {
+  cy.defaultLogin();
+});
+
+describe('settings for workflows', () => {
+  it('can enable an disable workflows', () => {
+    cy.visit('/admin/workflowSettings');
+    //cy.contains('checkbox').should('be.visible');
+    cy.get('input[type="checkbox"]').click();
+  })
+});
