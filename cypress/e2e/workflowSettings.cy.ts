1a05c1c9fdbaea884cc8629754367e2797ef34d6
Felix <uhvuh@student.kit.edu>
2024-08-09 14:44:23 +0200

    Updated Cypress test for workflowSettings

--- cypress/e2e/workflowSettings.cy.ts
+++ cypress/e2e/workflowSettings.cy.ts
@@ -5,7 +5,7 @@ before(() => {
 describe('settings for workflows', () => {
   it('can enable an disable workflows', () => {
     cy.visit('/admin/workflowSettings');
-    //cy.contains('checkbox').should('be.visible');
-    cy.get('input[type="checkbox"]').click();
+    cy.contains('checkbox').should('be.visible');
+    cy.get('checkbox').click();
   });
 });
