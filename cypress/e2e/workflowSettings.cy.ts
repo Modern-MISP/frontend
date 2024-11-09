347f5196a045595f17bc2785cb442948f4914abc
Konstantin Zangerle <konstantin.zangerle@kit.edu>
2024-11-09 01:35:01 +0100

    disable workflowSettings test for legacy misp

--- cypress/e2e/workflowSettings.cy.ts
+++ cypress/e2e/workflowSettings.cy.ts
@@ -2,12 +2,14 @@ before(() => {
   cy.defaultLogin();
 });
 
-describe('settings for workflows', () => {
-  it('can enable an disable workflows', () => {
-    cy.visit('/admin/workflowSettings');
-    cy.contains('Execution of workflows is allowed:')
-      .parent()
-      .find('input[type="checkbox"]')
-      .check({ force: true });
+if (!Cypress.env('legacy')) {
+  describe('settings for workflows', () => {
+    it('can enable an disable workflows', () => {
+      cy.visit('/admin/workflowSettings');
+      cy.contains('Execution of workflows is allowed:')
+        .parent()
+        .find('input[type="checkbox"]')
+        .check({ force: true });
+    });
   });
-});
+}
