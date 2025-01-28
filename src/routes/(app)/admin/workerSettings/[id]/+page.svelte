6d504e37822a4ab82c3ae3787b44a214283d9a55
uxnuk <uxnuk@student.kit.edu>
2025-01-28 16:14:29 +0000

    del returning jobs table in single worker view

--- src/routes/(app)/admin/workerSettings/[id]/+page.svelte
+++ src/routes/(app)/admin/workerSettings/[id]/+page.svelte
@@ -5,15 +5,31 @@
   /** Page data containing data of the users with the 'id' in the url */
   export let data: PageData;
 
-  $: ({ left, right, user } = data);
+  $: ({ jobs, jobs_header, jobqueues, jobqueues_header, queueEditActions } = data);
 </script>
 
 <!--
   @component
   Displays information about a specific worker, specified by `id`.
 -->
-
-<div class="flex flex-wrap w-full h-full gap-2 lg:flex-nowrap">
-  <DynCard header={left} data={user} />
-  <DynCard header={right} data={user} />
+<div class="flex-wrap w-full h-full gap-2 lg:flex-nowrap">
+  <SideMenuEntry href="/logs/" name="Logs" icon="mdi:tag" isMenuOpen={true}></SideMenuEntry>
+  <div class="flex flex-wrap w-full h-full gap-2 lg:flex-nowrap">
+    <div>
+      <ComplexTableLayout
+        header={jobs_header}
+        tableData={jobs}
+        tableHref={() => undefined}
+        editActions={[]}
+      />
+    </div>
+    <div>
+      <ComplexTableLayout
+        header={jobqueues_header}
+        tableData={jobqueues}
+        tableHref={() => undefined}
+        editActions={queueEditActions}
+      />
+    </div>
+  </div>
 </div>
--- src/routes/(app)/admin/workerSettings/[id]/+page.ts
+++ src/routes/(app)/admin/workerSettings/[id]/+page.ts
@@ -28,24 +28,12 @@ export const load: PageLoad = async ({ params, fetch }) => {
     params: { path: { id: params.id } },
     fetch
   });
-  const {
-    data: returningJobs,
-    error: mispErrorReturningJobs,
-    response: responseReturningJobs
-  } = await get(api).GET('/worker/returningJobs/{id}', {
-    params: { path: { id: params.id } },
-    fetch
-  });
+
   // eslint-disable-next-line no-warning-comments
   //TODO: type message not set, therefor .message default to never
   if (mispErrorJobqueues)
     error(responseJobqueues.status as NumericRange<400, 599>, mispErrorJobqueues['message']);
   if (mispErrorJobs) error(responseJobs.status as NumericRange<400, 599>, mispErrorJobs['message']);
-  if (mispErrorReturningJobs)
-    error(
-      responseReturningJobs.status as NumericRange<400, 599>,
-      mispErrorReturningJobs['message']
-    );
 
   const left = [].filter((x) => typeof x !== 'undefined');
 
@@ -114,8 +102,6 @@ export const load: PageLoad = async ({ params, fetch }) => {
   return {
     jobs,
     jobs_header,
-    returningJobs,
-    returningJobs_header,
     jobqueues,
     jobqueues_header,
     queueEditActions
